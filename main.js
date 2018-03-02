8020ea00e908715efc04891c59ad5cfecfd688cf
Brian Crump <briancrump@Brians-MacBook-Pro.local>
2018-03-02 13:42:15 -0500

    created game logic

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardThree);
+
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = []; 
+
+var checkForMath = function () {
+	if (cardsInPlay [0] === cardsInPlay [1]) {
+	console.log("You Found A Match!");
+	} else { 
+		console.log("Sorry, try again.");
+}
+};
+
+
+var flipCard = function(cardId) {
+	console.log("User flipped" + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if(cardsInPlay.length === 2) {
+		checkForMath ();
+	}
+};
+
+
+
+
+
+flipCard(0);
+flipCard(2);
