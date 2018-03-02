469f66dd78e2b36c7e8f2b47521f458316c5818c
Brian Crump <briancrump@Brians-MacBook-Pro.local>
2018-03-02 18:04:58 -0500

    created game logic

--- main.js
+++ main.js
@@ -3,21 +3,47 @@ console.log("Up and running!");
 //console.log("User flipped " + cardOne);
 //console.log("User flipped " + cardThree);
 
-var cards = ["queen","queen","king","king"];
+//in the cards array create 4 objects, one for each card
+var cards = [ 
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{ 
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+
 var cardsInPlay = []; 
 
 var checkForMath = function () {
 	if (cardsInPlay [0] === cardsInPlay [1]) {
 	console.log("You Found A Match!");
 	} else { 
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 }
 };
 
 
 var flipCard = function(cardId) {
-	console.log("User flipped" + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
 	if(cardsInPlay.length === 2) {
 		checkForMath ();
 	}
@@ -29,3 +55,4 @@ var flipCard = function(cardId) {
 
 flipCard(0);
 flipCard(2);
+
